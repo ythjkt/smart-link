a06c7a5c9e8e30ff5cca18887705e26686241342
bebaty01 <ythjkt@gmail.com>
2018-09-25 20:17:49 +0900

    Adds http:// to href

--- client/src/components/LinkListItem.js
+++ client/src/components/LinkListItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 const LinkListItem = ({ link, onDelete }) => {
   const { _id, smartLink, targetUrl, createdAt }  = link;
   return (
-    <li>{_id} - <a href={smartLink}>{smartLink}</a> - {targetUrl} - {createdAt}<button onClick={onDelete} id={_id}>Remove</button></li>
+    <li>{_id} - <a href={'http://' + smartLink}>{smartLink}</a> - {targetUrl} - {createdAt}<button onClick={onDelete} id={_id}>Remove</button></li>
   );
 };
 
