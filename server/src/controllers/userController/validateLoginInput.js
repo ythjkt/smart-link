1c449da4d0811a1145e7c408e813afc94a5be09d
bebaty01 <ythjkt@gmail.com>
2018-10-07 22:27:57 +0900

    Removes console.log for testing

--- server/src/controllers/userController/validateLoginInput.js
+++ server/src/controllers/userController/validateLoginInput.js
@@ -8,16 +8,15 @@ module.exports = function validateLoginInput(data) {
     password = ''
   } = data
 
-  if (Validator.isEmpty(email)) {
-    errors.email = 'Email field is required'
-  }
-
   if (!Validator.isEmail(email)) {
     errors.email = 'Invalid email'
   }
 
+  if (Validator.isEmpty(email)) {
+    errors.email = 'Email field is required'
+  }
+
   if (Validator.isEmpty(password)) {
-    console.log('checking password')
     errors.password = 'Password field is required'
   }
 
