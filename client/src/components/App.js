33aa388fa3c3e82f552c17735f45eb0470d2497d
bebaty01 <ythjkt@gmail.com>
2018-10-17 18:49:46 +0900

    Adds redux

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import jwt_decode from 'jwt-decode'
 import store from '../store'
+
+import setAuthToken from '../utils/setAuthToken'
+
+// Import actions
+import { setCurrentUser, logoutUser } from '../actions/authActions'
 
 // Import compenents
 import Header from './Header'
 import Landing from './Landing'
 import Register from './Register'
 import Login from './Login'
+
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header
+  setAuthToken(localStorage.jwtToken)
+  // Decode token get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken)
+  // set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded))
+  // check for expired user
+  const currentTime = Date.now() / 1000
+  if (decoded.exp < currentTime) {
+    store.dispatch(logoutUser())
+
+    // TODO: Clear profile
+
+    // Redirect to login
+    window.location.href = '/login'
+  }
+}
 
 class App extends Component {
   render() {
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,26 +3,53 @@ import { Link } from 'react-router-dom'
 import PropTyps from 'prop-types'
 import { connect } from 'react-redux'
 
+import { logoutUser } from '../actions/authActions'
+
 class Header extends Component {
+  onLogoutClick(e) {
+    e.preventDefault()
+    this.props.logoutUser()
+  }
+
   render() {
+    const { isAuthenticated, user } = this.props.auth
+
+    const authLinks = (
+      <ul>
+        <li>
+          <a href="" onClick={() => this.onLogoutClick()}>
+            Logout
+          </a>
+        </li>
+      </ul>
+    )
+    const guestLinks = (
+      <ul>
+        <li>
+          <Link to="/register">Sign Up</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    )
+
     return (
       <nav>
         <div>
           <Link to="/">SmartLink</Link>
         </div>
-        <div>
-          <ul>
-            <li>
-              <Link to="/register">Sign Up</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </div>
+        <div>{isAuthenticated ? authLinks : guestLinks}</div>
       </nav>
     )
   }
 }
 
-export default Header
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+
+export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(Header)
